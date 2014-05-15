627038646a33137dc592105e7a279eefda54b387
xMEGA <belorusprom@gmail.com>
2014-05-15 23:51:09 +0300

    Display tabs deleted

--- Windows/src/Display/Display.h
+++ Windows/src/Display/Display.h
@@ -15,14 +15,13 @@ class Display_t
 {
     public:
         void Init( uint16_t xSize, uint16_t ySize );
-		void WindowResize( uint16_t xSize, uint16_t ySize );
-		
+        void WindowResize( uint16_t xSize, uint16_t ySize );
 
         void Present( PixelStruct_t* pData );
         void DrawPixel( uint16_t xPos, uint16_t yPos, uint8_t red, uint8_t green, uint8_t blue );
         void Flip( void );
         void Clear();
-		~Display_t();
+        ~Display_t();
 
     private:
         void SurfaceLock( void );
@@ -30,14 +29,14 @@ class Display_t
         
 
     private:
-		SDL_Window*    m_pWindow;   
-		SDL_GLContext  m_pOpenGLContext;
-		PixelStruct_t* m_pFrame[ 2 ];
+        SDL_Window*    m_pWindow;   
+        SDL_GLContext  m_pOpenGLContext;
+        PixelStruct_t* m_pFrame[ 2 ];
         uint16_t       m_WindowSizeX;
         uint16_t       m_WindowSizeY;
         uint16_t       m_SurfaceSizeX;
         uint16_t       m_SurfaceSizeY;
-		uint8_t        m_CurrentFrame;
+        uint8_t        m_CurrentFrame;
 };
 
 #endif
