75211c6855387fb7cd3ebd846f1c144dec36e133
Volodymyr Davydenko <davydvo@gmail.com>
2014-09-26 16:46:56 +0300

    [PS2Keyboard] misc

--- platform/OscesBoard/System/Keyboard/PS2KeyboardPlatform.h
+++ platform/OscesBoard/System/Keyboard/PS2KeyboardPlatform.h
@@ -21,6 +21,12 @@ private:
 private:
     GpioPin_t* m_DataPin;
     GpioPin_t* m_ClockPin;
+
+private:
+    uint8_t m_started;
+    uint8_t m_bitCount;
+    uint8_t m_data;
+    uint8_t m_parity;
 };
 
 #endif // KEYBOARD_H
