27da8eda76047254a26d83a3c08d8b964fd41eeb
Volodymyr Davydenko <davydvo@gmail.com>
2017-11-25 21:50:00 +0200

    Add schematics

---
+++ hardware/schematics/PinAssigningScript.tcl
@@ -0,0 +1,54 @@
+set_location_assignment	PIN_83   -to VGA_R[0]
+set_location_assignment	PIN_82   -to VGA_R[1]
+set_location_assignment	PIN_81   -to VGA_R[2]
+set_location_assignment	PIN_78   -to VGA_R[3]
+set_location_assignment	PIN_76   -to VGA_R[4]
+
+set_location_assignment	PIN_77   -to VGA_G[0]
+set_location_assignment	PIN_75   -to VGA_G[1]
+set_location_assignment	PIN_73   -to VGA_G[2]
+set_location_assignment	PIN_71   -to VGA_G[3]
+set_location_assignment	PIN_69   -to VGA_G[4]
+set_location_assignment	PIN_67   -to VGA_G[5]
+
+set_location_assignment	PIN_66   -to VGA_B[0]
+set_location_assignment	PIN_68   -to VGA_B[1]
+set_location_assignment	PIN_70   -to VGA_B[2]
+set_location_assignment	PIN_72   -to VGA_B[3]
+set_location_assignment	PIN_74   -to VGA_B[4]
+
+set_location_assignment	PIN_84   -to VGA_HS
+set_location_assignment	PIN_85   -to VGA_VS
+
+set_location_assignment	PIN_42   -to SRAM_A[0]
+set_location_assignment	PIN_47   -to SRAM_A[1]
+set_location_assignment	PIN_48   -to SRAM_A[2]
+set_location_assignment	PIN_49   -to SRAM_A[3]
+set_location_assignment	PIN_50   -to SRAM_A[4]
+set_location_assignment	PIN_57   -to SRAM_A[5]
+set_location_assignment	PIN_58   -to SRAM_A[6]
+set_location_assignment	PIN_61   -to SRAM_A[7]
+set_location_assignment	PIN_62   -to SRAM_A[8]
+set_location_assignment	PIN_64   -to SRAM_A[9]
+set_location_assignment	PIN_41   -to SRAM_A[10]
+set_location_assignment	PIN_40   -to SRAM_A[11]
+set_location_assignment	PIN_39   -to SRAM_A[12]
+set_location_assignment	PIN_38   -to SRAM_A[13]
+set_location_assignment	PIN_37   -to SRAM_A[14]
+set_location_assignment	PIN_29   -to SRAM_A[15]
+set_location_assignment	PIN_28   -to SRAM_A[16]
+set_location_assignment	PIN_27   -to SRAM_A[17]
+set_location_assignment	PIN_26   -to SRAM_A[18]
+
+set_location_assignment	PIN_52   -to SRAM_IO[0]
+set_location_assignment	PIN_53   -to SRAM_IO[1]
+set_location_assignment	PIN_54   -to SRAM_IO[2]
+set_location_assignment	PIN_55   -to SRAM_IO[3]
+set_location_assignment	PIN_36   -to SRAM_IO[4]
+set_location_assignment	PIN_35   -to SRAM_IO[5]
+set_location_assignment	PIN_34   -to SRAM_IO[6]
+set_location_assignment	PIN_33   -to SRAM_IO[7]
+
+set_location_assignment	PIN_51   -to SRAM_CE
+set_location_assignment	PIN_56   -to SRAM_WE
+set_location_assignment	PIN_30   -to SRAM_OE
